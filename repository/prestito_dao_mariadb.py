d8bff2793fecbfe9669059142d3cb9e9d61b23af
PySimon25 <213518354+PySimon25@users.noreply.github.com>
2025-07-22 00:14:00 +0200

    lendings dao fill all objects data

--- repository/prestito_dao_mariadb.py
+++ repository/prestito_dao_mariadb.py
@@ -1,5 +1,5 @@
 import mariadb
-from dto import Biblioteca, Bibliotecario, Libro, Prestito, Utente
+from dto import Biblioteca, Bibliotecario, Categoria, Genere, Libro, Prestito, Utente
 from repository import PrestitoDAOInterface
 
 # Implementazione concreta del DAO
@@ -34,13 +34,22 @@ class PrestitoDAOMariaDB(PrestitoDAOInterface):
             with conn.cursor(dictionary=True) as cursor:
                 cursor.execute("""SELECT pr.id_prestito, pr.data_prestito, pr.data_restituzione_prevista, pr.data_restituzione_effettiva, 
                                ut.id_utente, ut.nome nome_utente, ut.cognome cognome_utente, ut.email, ut.telefono, li.id_libro, li.titolo, 
-                               li.autore, li.anno_pubblicazione, li.isbn, bb.id_biblioteca, bb.nome_biblioteca, bb.città citta, bb.indirizzo, 
-                               bi.id_bibliotecario, bi.nome nome_bibliotecario, bi.cognome cognome_bibliotecario, bi.email
+                               li.autore, li.anno_pubblicazione,  li.isbn, ca.id_categoria, ca.nome_categoria, ge.id_genere, ge.nome_genere,
+                               bb.id_biblioteca, bb.nome_biblioteca, bb.città citta, bb.indirizzo,
+                               bbl.id_biblioteca libro_biblioteca_id_biblioteca, bbl.nome_biblioteca libro_biblioteca_nome_biblioteca, 
+                               bbl.città libro_biblioteca_citta, bbl.indirizzo libro_biblioteca_indirizzo,
+                               bi.id_bibliotecario, bi.nome nome_bibliotecario, bi.cognome cognome_bibliotecario, bi.email email_bibliotecario,
+                               bbb.id_biblioteca bibliotecario_biblioteca_id_biblioteca, bbb.nome_biblioteca bibliotecario_biblioteca_nome_biblioteca,
+                               bbb.città bibliotecario_biblioteca_citta, bbb.indirizzo bibliotecario_biblioteca_indirizzo
                                FROM Prestiti pr 
                                JOIN Utenti ut on ut.id_utente = pr.id_utente 
                                JOIN Libri li on pr.id_libro = li.id_libro 
+                               JOIN Biblioteche bbl on bbl.id_biblioteca = li.id_biblioteca 
                                JOIN Biblioteche bb on bb.id_biblioteca = pr.id_biblioteca
                                JOIN Bibliotecari bi on bi.id_bibliotecario = pr.id_bibliotecario
+                               JOIN Biblioteche bbb on bbb.id_biblioteca = bi.id_biblioteca
+                               JOIN Categorie ca on ca.id_categoria = li.id_categoria
+                               JOIN Generi ge on ge.id_genere = li.id_genere
                                WHERE id_prestito = ?""", (id_prestito,))
                 row = cursor.fetchone()
                 return self._row_to_prestito(row)
@@ -54,13 +63,22 @@ class PrestitoDAOMariaDB(PrestitoDAOInterface):
             with conn.cursor(dictionary=True) as cursor:
                 cursor.execute("""SELECT pr.id_prestito, pr.data_prestito, pr.data_restituzione_prevista, pr.data_restituzione_effettiva, 
                                ut.id_utente, ut.nome nome_utente, ut.cognome cognome_utente, ut.email, ut.telefono, li.id_libro, li.titolo, 
-                               li.autore, li.anno_pubblicazione,  li.isbn, bb.id_biblioteca, bb.nome_biblioteca, bb.città citta, bb.indirizzo, 
-                               bi.id_bibliotecario, bi.nome nome_bibliotecario, bi.cognome cognome_bibliotecario, bi.email
+                               li.autore, li.anno_pubblicazione,  li.isbn, ca.id_categoria, ca.nome_categoria, ge.id_genere, ge.nome_genere,
+                               bb.id_biblioteca, bb.nome_biblioteca, bb.città citta, bb.indirizzo,
+                               bbl.id_biblioteca libro_biblioteca_id_biblioteca, bbl.nome_biblioteca libro_biblioteca_nome_biblioteca, 
+                               bbl.città libro_biblioteca_citta, bbl.indirizzo libro_biblioteca_indirizzo,
+                               bi.id_bibliotecario, bi.nome nome_bibliotecario, bi.cognome cognome_bibliotecario, bi.email email_bibliotecario,
+                               bbb.id_biblioteca bibliotecario_biblioteca_id_biblioteca, bbb.nome_biblioteca bibliotecario_biblioteca_nome_biblioteca,
+                               bbb.città bibliotecario_biblioteca_citta, bbb.indirizzo bibliotecario_biblioteca_indirizzo
                                FROM Prestiti pr 
                                JOIN Utenti ut on ut.id_utente = pr.id_utente 
                                JOIN Libri li on pr.id_libro = li.id_libro 
+                               JOIN Biblioteche bbl on bbl.id_biblioteca = li.id_biblioteca 
                                JOIN Biblioteche bb on bb.id_biblioteca = pr.id_biblioteca
                                JOIN Bibliotecari bi on bi.id_bibliotecario = pr.id_bibliotecario
+                               JOIN Biblioteche bbb on bbb.id_biblioteca = bi.id_biblioteca
+                               JOIN Categorie ca on ca.id_categoria = li.id_categoria
+                               JOIN Generi ge on ge.id_genere = li.id_genere
                                WHERE pr.id_utente = ?""", (id_utente,))
                 rows = cursor.fetchall()
                 return [self._row_to_prestito(row) for row in rows]
@@ -70,6 +88,8 @@ class PrestitoDAOMariaDB(PrestitoDAOInterface):
     def _row_to_prestito(self, row: dict) -> Prestito:
         # Per popolare oggetti complessi non è possibile usare
         # return Prestito(**row) if row else None  /  return [Prestito(**row) for row in rows]
+
+        # Utente al quale è stato concesso il Prestito
         utente = Utente(
             id_utente = row['id_utente'],
             nome = row['nome_utente'],
@@ -78,6 +98,7 @@ class PrestitoDAOMariaDB(PrestitoDAOInterface):
             telefono = row['telefono']
         )
 
+        # Biblioteca associata al Prestito
         biblioteca = Biblioteca(
             id_biblioteca = row['id_biblioteca'],
             nome_biblioteca = row['nome_biblioteca'],
@@ -85,20 +106,56 @@ class PrestitoDAOMariaDB(PrestitoDAOInterface):
             indirizzo = row['indirizzo']
         )
 
-        bibliotecario = Bibliotecario(
-            id_bibliotecario = row['id_bibliotecario'],
-            nome = row['nome_bibliotecario'],
-            cognome = row['cognome_bibliotecario']
+        # Biblioteca alla quale è assegnato il bibliotecario
+        bibliotecario_biblioteca = Biblioteca( 
+            id_biblioteca = row['bibliotecario_biblioteca_id_biblioteca'],
+            nome_biblioteca = row['bibliotecario_biblioteca_nome_biblioteca'],
+            citta = row['bibliotecario_biblioteca_citta'],
+            indirizzo = row['bibliotecario_biblioteca_indirizzo']
         )
 
+        # Bibliotecario che ha concesso il Prestito
+        bibliotecario = Bibliotecario(
+            id_bibliotecario = row['id_bibliotecario'],
+            biblioteca = bibliotecario_biblioteca,
+            nome = row['nome_bibliotecario'],
+            cognome = row['cognome_bibliotecario'],
+            email = row['email_bibliotecario'] 
+        )
+
+        # Biblioteca alla quale è assegnato il Libro
+        libro_biblioteca = Biblioteca(
+            id_biblioteca = row['libro_biblioteca_id_biblioteca'],
+            nome_biblioteca = row['libro_biblioteca_nome_biblioteca'],
+            citta = row['libro_biblioteca_citta'],
+            indirizzo = row['libro_biblioteca_indirizzo']
+        )
+
+        # Categoria del Libro
+        categoria = Categoria(
+            id_categoria = row['id_categoria'],
+            nome_categoria = row['nome_categoria']
+        )
+
+        # Genere del Libro
+        genere = Genere(
+            id_genere = row['id_genere'],
+            nome_genere = row['nome_genere']
+        )
+
+        # Libro concesso in Prestito
         libro = Libro(
             id_libro = row['id_libro'],
+            biblioteca = libro_biblioteca,
             titolo = row['titolo'],
             autore = row['autore'],
             anno_pubblicazione = row['anno_pubblicazione'],
-            isbn = row['isbn']
+            isbn = row['isbn'],
+            genere = genere,
+            categoria = categoria
         )
 
+        # Dati del Prestito
         return Prestito(
             id_prestito = row['id_prestito'],
             biblioteca = biblioteca,
